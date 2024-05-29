029e4aa84edb27c534cfbd089246d912d77628b7
William Janssen <janssenwilliam02@gmail.com>
2024-05-29 19:31:36 +0200

    text ändring och linkedin

--- src/sections/header/data.js
+++ src/sections/header/data.js
@@ -1,13 +1,13 @@
 import {AiOutlineInstagram} from 'react-icons/ai'
 import {AiOutlineTwitter} from 'react-icons/ai'
-import {AiOutlineDribbble} from 'react-icons/ai'
+import { GrLinkedin } from "react-icons/gr";
 import {AiFillGithub} from 'react-icons/ai'
 
 const data = [
-    {id: 1, link: 'https://instagram.com', icon: <AiOutlineInstagram/>},
+    {id: 1, link: 'https://www.instagram.com/chasacademy/', icon: <AiOutlineInstagram/>},
     {id: 2, link: 'https://twitter.com', icon: <AiOutlineTwitter/>},
-    {id: 3, link: 'https://dribbble.com', icon: <AiOutlineDribbble/>},
-    {id: 4, link: 'https://github.com', icon: <AiFillGithub/>}
+    {id: 3, link: 'https://dribbble.com', icon: <GrLinkedin />},
+    {id: 4, link: 'https://github.com/William0209', icon: <AiFillGithub/>}
 ]
 
 export default data
