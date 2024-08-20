7d5c0ba4072121363ed4f381b1421b667772d210
William0209 <janssenwilliam02@gmail.com>
2024-08-20 18:17:11 +0200

    Fix

--- src/sections/header/Header.jsx
+++ src/sections/header/Header.jsx
@@ -1,37 +1,75 @@
-import { useEffect } from 'react'
-import HeaderImage from '../../assets/profilbild.jpg'
-import data from './data'
-import AOS from 'aos'
-import 'aos/dist/aos.css'
-import './header.css'
+import { useEffect } from "react";
+import HeaderImage from "../../assets/profilbild.jpg";
+import data from "./data";
+import AOS from "aos";
+import "aos/dist/aos.css";
+import "./header.css";
 
 const Header = () => {
   useEffect(() => {
-    AOS.init({duration: 2000})
-  }, [])
+    AOS.init({ duration: 2000 });
+  }, []);
 
   return (
     <header id="header">
       <div className="container header__container">
-        <div className="header__profile" data-aos="fade-in">
-          <img src={HeaderImage} alt="Header Portait" />
+        <div
+          className="header__profile"
+          data-aos="fade-in"
+        >
+          <img
+            src={HeaderImage}
+            alt="Header Portait"
+          />
         </div>
-        <h3  data-aos="fade-up">William Janssen</h3>
-        <p  data-aos="fade-up">
-          Welcome to my portfolio! I'm William Janssen, a passionate Fullstack Developer. Explore my projects and journey as I master the art of creating dynamic and innovative web applications.
+        <h3 data-aos="fade-up">
+          William Janssen
+        </h3>
+        <p data-aos="fade-up">
+          Welcome to my portfolio! I'm
+          William Janssen, a passionate
+          Fullstack Developer. Currently
+          searching for an internship in
+          the field of web development.
+          I'm always looking for new
+          challenges and opportunities
+          to grow. Feel free to reach
+          out to me if you have any
+          questions or would like to
+          work together.
         </p>
-        <div className="header__cta"  data-aos="fade-up">
-          <a href="#contact" className='btn primary'>Let's Talk</a>
-          <a href="#portfolio" className='btn light'>My Work</a>
+        <div
+          className="header__cta"
+          data-aos="fade-up"
+        >
+          <a
+            href="#contact"
+            className="btn primary"
+          >
+            Let's Talk
+          </a>
+          <a
+            href="#portfolio"
+            className="btn light"
+          >
+            My Work
+          </a>
         </div>
         <div className="header__socials">
-          {
-            data.map(item => <a key={item.id} href={item.link} target="_blank" rel="noopener noreferrer">{item.icon}</a>)
-          }
+          {data.map((item) => (
+            <a
+              key={item.id}
+              href={item.link}
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              {item.icon}
+            </a>
+          ))}
         </div>
       </div>
     </header>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
--- src/sections/services/Services.jsx
+++ src/sections/services/Services.jsx
@@ -1,27 +1,36 @@
-import Card from '../../components/Card'
-import data from './data'
-import './services.css'
+import Card from "../../components/Card";
+import data from "./data";
+import "./services.css";
 
 const Services = () => {
   return (
     <section id="services">
-      <h2>Proficiencies</h2>
-      <p>I give you the best in all the services below</p>
-      <div className="container services__container" data-aos="fade-up">
-        {
-          data.map(item => (
-            <Card key={item.id} className="service light">
-              <div className="service__icon">{item.icon}</div>
-              <div className="service__details">
-                <h4>{item.title}</h4>
-                  <p>{item.desc}</p>
-              </div>
-            </Card>
-          ))
-        }
+      <h2>Knowledge Areas</h2>
+      <p>
+        This is what i have been
+        focusing on
+      </p>
+      <div
+        className="container services__container"
+        data-aos="fade-up"
+      >
+        {data.map((item) => (
+          <Card
+            key={item.id}
+            className="service light"
+          >
+            <div className="service__icon">
+              {item.icon}
+            </div>
+            <div className="service__details">
+              <h4>{item.title}</h4>
+              <p>{item.desc}</p>
+            </div>
+          </Card>
+        ))}
       </div>
     </section>
-  )
-}
+  );
+};
 
-export default Services
+export default Services;
