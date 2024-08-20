1cef5d3b741d3bae5f53cdb1a3d3ac64239a9e8d
William0209 <janssenwilliam02@gmail.com>
2024-08-20 18:07:49 +0200

    Fix

--- src/sections/portfolio/Portfolio.jsx
+++ src/sections/portfolio/Portfolio.jsx
@@ -1,38 +1,61 @@
-import './portfolio.css'
-import Projects from './Projects'
-import ProjectsCategories from './ProjectsCategories'
-import data from './data'
-import { useState } from 'react'
+import "./portfolio.css";
+import Projects from "./Projects";
+import ProjectsCategories from "./ProjectsCategories";
+import data from "./data";
+import { useState } from "react";
 
 const Portfolio = () => {
-  const [projects, setProjects] = useState(data);
+  const [projects, setProjects] =
+    useState(data);
 
-  const categories = data.map(item => item.category);
-  const uniqueCategories = ["all", ...new Set(categories)];
-  
-  const filterProjectsHandler = (category) => {
-    if(category === "all") {
+  const categories = data.map(
+    (item) => item.category
+  );
+  const uniqueCategories = [
+    "all",
+    ...new Set(categories),
+  ];
+
+  const filterProjectsHandler = (
+    category
+  ) => {
+    if (category === "all") {
       setProjects(data);
       return;
     }
 
-    const filterProjects = data.filter(project => project.category === category);
+    const filterProjects = data.filter(
+      (project) =>
+        project.category === category
+    );
     setProjects(filterProjects);
-  }
-  
+  };
 
   return (
     <section id="portfolio">
-      <h2>Recent Projects</h2>
+      <h2>
+        Stay tuned for upcoming projects
+      </h2>
       <p>
-      Explore some of the projects I've recently completed. Please note that the projects below are placeholders and will soon be replaced with real examples. Use the buttons to navigate through different categories.
+        Explore some of the projects
+        I've recently completed. Please
+        note that the projects below are
+        placeholders and will soon be
+        replaced with real examples. Use
+        the buttons to navigate through
+        different categories.
       </p>
       <div className="container portfolio__container">
-        <ProjectsCategories categories={uniqueCategories} onFilterProjects={filterProjectsHandler}/>
-        <Projects projects={projects}/>
+        <ProjectsCategories
+          categories={uniqueCategories}
+          onFilterProjects={
+            filterProjectsHandler
+          }
+        />
+        <Projects projects={projects} />
       </div>
     </section>
-  )
-}
+  );
+};
 
-export default Portfolio
+export default Portfolio;
